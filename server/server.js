5a7db39a9a0a1017dfffc610c27e4b92725e1d79
KevinRhode <77312186+KevinRhode@users.noreply.github.com>
2024-07-26 13:06:44 -0400

    add: babel for older browsers

--- server/server.js
+++ server/server.js
@@ -1,3 +1,6 @@
+//import 'core-js/stable';
+//import 'regenerator-runtime/runtime';
+
 const express = require("express");
 const { ApolloServer } = require("@apollo/server");
 const { expressMiddleware } = require("@apollo/server/express4");
